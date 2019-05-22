fe30471374dc0b7a4510640121c48ef17156d7e8
vitor <vitorhugoemai@gmail.com>
2019-05-22 20:03:09 -0300

    mudar tempo

--- instrumento.js
+++ instrumento.js
@@ -2,7 +2,7 @@
 let botao=document.querySelector('#solo');
 botao.addEventListener('click',function(){
   let audio=document.querySelector('audio');
-  audio.currentTime=10
+  audio.currentTime=19
   audio.controls;
   audio.play();
 });
