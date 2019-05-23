382fe696bb1e992fd2f727129f27ff142853b3b9
vitorhugoinf <40009988+vitorhugoinf@users.noreply.github.com>
2019-05-23 19:43:03 -0300

    Add files via upload

--- instrumento.js
+++ instrumento.js
@@ -3,6 +3,7 @@ let botao=document.querySelector('#solo');
 botao.addEventListener('click',function(){
   let audio=document.querySelector('audio');
   audio.currentTime=19
-  audio.controls;
   audio.play();
+audio.controls();
+  audio.pause();
 });
